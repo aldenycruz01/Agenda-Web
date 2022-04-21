46c6d56ad52445cd743a81b0d85257fc13a19efc
aldenycruz01 <aldenysix@gmail.com>
2022-04-21 17:34:10 -0300

    Commiti inicial

---
+++ WebAgenda/Data/BancoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAgenda.Models;
+
+namespace WebAgenda.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext>options) : base(options)  
+        {
+
+        }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+            
+    }
+}
---
+++ WebAgenda/Models/ContatoModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAgenda.Models
+{
+    public class ContatoModel
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+
+        public string email { get; set; }
+        public int celular { get; set; }
+
+        
+    }
+}
